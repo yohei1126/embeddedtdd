12dc0c499ddd884ece14afb3f0fc228f1390ba5b
U-yohei-PC\yohei <yohei@yohei-PC.(none)>
2012-01-08 00:20:15 +0900

    first test for sprintf

---
+++ unity/stdio/SprintfTest.c
@@ -0,0 +1,17 @@
+#include "unity_fixture.h"
+#include "stdio.h"
+
+TEST_SETUP(sprintf)
+{
+}
+
+TEST_TEAR_DOWN(sprintf)
+{
+}
+
+TEST(sprintf, NoFormatOperations)
+{
+    char output[5];
+    TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
+    TEST_ASSERT_EQUAL_STRING("hey", output);
+}
