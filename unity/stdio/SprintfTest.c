043f3aab50bc7aefdae700d29dc753c385f832bb
U-yohei-PC\yohei <yohei@U30JC.(none)>
2012-01-21 00:54:48 +0900

    add Test No Format Operations and check buffer overrun

--- unity/stdio/SprintfTest.c
+++ unity/stdio/SprintfTest.c
@@ -1,5 +1,6 @@
 #include "unity_fixture.h"
 #include "stdio.h"
+#include "string.h"
 
 TEST_SETUP(sprintf)
 {
@@ -11,14 +12,37 @@ TEST_TEAR_DOWN(sprintf)
 
 TEST(sprintf, NoFormatOperations)
 {
-    char output[5];
-    TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
-    TEST_ASSERT_EQUAL_STRING("hey", output);
+  char output[5];
+  TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
+  TEST_ASSERT_EQUAL_STRING("hey", output);
 }
 
-TEST(sprintf, NoFormatOperationsWithInitialization)
+TEST(sprintf, NoFormatOperations2)
 {
-    char output[5] = "";
-    TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
-    TEST_ASSERT_EQUAL_STRING("hey", output);
+  char output[5] = "";
+  TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
+  TEST_ASSERT_EQUAL_STRING("hey", output);
 }
+
+TEST(sprintf, InsertString)
+{
+  char output[20] = "";
+  TEST_ASSERT_EQUAL(12, sprintf(output, "Hello %s\n", "World"));
+  TEST_ASSERT_EQUAL_STRING("Hello World\n", output);
+}
+
+TEST(sprintf, NoFormatOperations3)
+{
+  //NoFormatOperation2 do not guard againt sprintf 
+  //writing past the string terminator.
+
+  char output[5];
+  memset(output, 0xaa, sizeof output);
+  TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
+  TEST_ASSERT_EQUAL_STRING("hey", output);
+  //check the that the character after the terminationg null
+  //is not changed.
+  TEST_ASSERT_BYTES_EQUAL(0xaa, output[4]);
+}
+
+
