82da2279b5ee1dd701e9640ba487c1c88cc2cd89
U-yohei-PC\yohei <yohei@U30JC.(none)>
2012-02-20 20:59:51 +0900

    add LedDriver test

--- unity/AllTests.c
+++ unity/AllTests.c
@@ -2,7 +2,8 @@
 
 static void RunAllTests(void)
 {
-    RUN_TEST_GROUP(sprintf);
+  RUN_TEST_GROUP(sprintf);
+  RUN_TEST_GROUP(LedDriver);
 }
 
 int main(int argc, char * argv[])
---
+++ unity/LedDriver/LedDriverTestRunner.c
@@ -0,0 +1,7 @@
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(LedDriver)
+{
+  RUN_TEST_CASE(LedDriver, LedsOffAfterCreate);
+}
+
