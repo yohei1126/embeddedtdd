eb200e889565d81d04c5e655ac4c4cfb163cbcc0
Yohei Onishi <vivre214@gmail.com>
2012-04-28 17:52:27 +0900

    add header fire to remove compile error

---
+++ include/HomeAutomation/LightScheduler.h
@@ -0,0 +1,12 @@
+#ifndef D_LIGHT_SCHEDULER_H
+#define D_LIGHT_SCHEDULER_H
+
+typedef enum {
+    NONE=-1, EVERYDAY=10, WEEKDAY, WEEKEND,
+    SUNDAY=1, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+} Day;
+
+void LightScheduler_ScheduleTurnOn(int id, Day day, int minute);
+void LightScheduler_Wakeup(void);
+
+#endif
